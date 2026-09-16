b2a15a826ce7d774fec5cbf1633433571dc96dcb
hvdeath <hvdeath@gmail.com>
2026-09-16 14:48:50 +0200

    Tests: configure WebApplicationFactory to use InMemory ApplicationDbContext

--- tests/Alerting.Api.Tests/AlertsControllerTests.cs
+++ tests/Alerting.Api.Tests/AlertsControllerTests.cs
@@ -1,43 +1,44 @@
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Alerting.Api.DTOs;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Alerting.Api.Tests
 {
-    public class DebugAlertsControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    public class AlertsControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly HttpClient _client;
-        private readonly ITestOutputHelper _output;
 
-        public DebugAlertsControllerTests(WebApplicationFactory<Program> factory, ITestOutputHelper output)
+        public AlertsControllerTests(WebApplicationFactory<Program> factory)
         {
             var configured = factory.WithWebHostBuilder(builder =>
             {
                 builder.ConfigureServices(services =>
                 {
+                    // Replace ApplicationDbContext with InMemory for tests
                     var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
                     if (descriptor != null) services.Remove(descriptor);
                     services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("AlertsTestDb"));
                 });
             });
-
             _client = configured.CreateClient();
-            _output = output;
         }
 
         [Fact]
-        public async Task Post_ReturnsBodyOnFailure()
+        public async Task Post_Then_Get_ReturnsCreated()
         {
-            var create = new CreateAlertRuleDto { Name = "Debug Alert", IsActive = true };
+            var create = new CreateAlertRuleDto { Name = "Test Alert", IsActive = true };
             var postResp = await _client.PostAsJsonAsync("/api/alerts", create);
-            var body = await postResp.Content.ReadAsStringAsync();
-            _output.WriteLine("ResponseStatus:" + postResp.StatusCode);
-            _output.WriteLine("ResponseBody:" + body);
-            Assert.True(postResp.IsSuccessStatusCode, body);
+            Assert.Equal(HttpStatusCode.Created, postResp.StatusCode);
+
+            var created = await postResp.Content.ReadFromJsonAsync<AlertRuleDto>();
+            Assert.NotNull(created);
+
+            var getResp = await _client.GetAsync($"/api/alerts/{created!.Id}");
+            Assert.Equal(HttpStatusCode.OK, getResp.StatusCode);
         }
     }
 }
