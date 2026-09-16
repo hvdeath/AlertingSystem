3d4c26311d9bb4438bbc3b4965637a08cb5082cc
hvdeath <hvdeath@gmail.com>
2026-09-16 12:57:26 +0200

    Fix ApplicationDbContext identity type reference to ApplicationUser

--- src/Alerting.Infrastructure/ApplicationDbContext.cs
+++ src/Alerting.Infrastructure/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 using Alerting.Domain.Entities;
+using Alerting.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Alerting.Infrastructure;
 
-public class ApplicationDbContext : IdentityDbContext<Identity.Identity.ApplicationUser>
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
